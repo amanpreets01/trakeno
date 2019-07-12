9be0342a3e32784183e46aabe5dd9dedec303a43
Flx0101 <codevelop0101@gmail.com>
2019-07-12 19:57:31 +0530

    Changes

--- pixel/server.js
+++ pixel/server.js
@@ -7,6 +7,10 @@ app.engine('html', require('hbs').__express);
 app.get('/get_pixel' , (req,res) => {
 	res.render('pixel.html');
 });
+
+app.get('/thank_you',(req,res) => {
+	res.render('thank.html');
+});
 app.listen(3000 , () => {
 	console.log(`Listening on 3000`)
 });
